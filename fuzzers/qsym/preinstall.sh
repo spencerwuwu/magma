ef40b048fab9ff0918ad04916615e3b89bf63e48
sojhal <sojhal@gmail.com>
2021-04-19 15:37:29 -0700

    QSYM and AFL running together successfully.

--- fuzzers/qsym/preinstall.sh
+++ fuzzers/qsym/preinstall.sh
@@ -13,26 +13,26 @@ curl -SL http://releases.llvm.org/9.0.0/clang%2bllvm-9.0.0-x86_64-linux-gnu-ubun
 mv clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04 clang_9.0.0
 sudo mv clang_9.0.0 /usr/local
 
-export PATH=/usr/local/clang_9.0.0/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/clang_9.0.0/lib:$LD_LIBRARY_PATH
+#export PATH=/usr/local/clang_9.0.0/bin:$PATH
+#export LD_LIBRARY_PATH=/usr/local/clang_9.0.0/lib:$LD_LIBRARY_PATH
 
 
 
 # Install QSYM fuzzer
-#git clone https://github.com/sojhal/qsym.git
+git clone https://github.com/sojhal/qsym.git
 
-#pushd qsym
+pushd qsym
 # disable ptrace_scope for PIN
 #echo 0|sudo tee /proc/sys/kernel/yama/ptrace_scope
 
 # install z3 and system deps
-#./setup.sh
+./setup.sh
 
 # install using virtual env
 # virtualenv venv
 # source venv/bin/activate
-#pip install .
-#popd
+pip install .
+popd
 
 # Install AFL
 #git clone https://github.com/google/AFL.git
